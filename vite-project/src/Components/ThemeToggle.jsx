349c19d6bdc4553f4aa0071c80b3fc15246c366a
Musse-A <ayan.m4@icloud.com>
2025-09-27 06:19:00 -0400

    Toggle between light and dark mode finished with preference stored locally

--- vite-project/src/Components/ThemeToggle.jsx
+++ vite-project/src/Components/ThemeToggle.jsx
@@ -1,7 +1,44 @@
 import { Moon, Sun } from 'lucide-react';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 export const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
-    return <button> {isDarkMode ? <Sun/> : <Moon />}</button>;
+   
+// Remembers and stores users's light/dark mode preference
+   useEffect(() => {
+    const storedTheme = localStorage.getItem('theme')
+    if (storedTheme === 'dark') {
+        setIsDarkMode(true);
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+        setIsDarkMode(false);
+    }
+   }
+   , []);
+
+    const toggleTheme = () => {
+    if (isDarkMode) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+        setIsDarkMode(false);
+    }else {     
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+        setIsDarkMode(true);
+    }
+   }
+
+//Button to switch between light and dark mode
+    return (
+    <button onClick={toggleTheme}> 
+        {""}
+        {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+        ) : (
+        <Moon className="h-6 w-6 text-yellow-900" />
+    )}
+    </button>
+    );
+
 };
