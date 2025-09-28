aacbd689db5474f49045626c6724d2997f75b27c
Musse-A <ayan.m4@icloud.com>
2025-09-28 03:58:17 -0400

    fix

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,15 +1,20 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  root: './vite-project', // Set this to where index.html lives
   base: process.env.VITE_BASE_PATH || "/Portfolio_V1",
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './vite-project/src'), // Adjust alias to src inside vite-project
     },
+  },
+  build: {
+    outDir: '../dist', // Output the build outside vite-project to the dist folder
+    emptyOutDir: true, // Clean output directory before build
   },
 })
