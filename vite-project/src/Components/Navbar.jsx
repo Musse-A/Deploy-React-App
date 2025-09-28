cfd9421c388c5004f428174579b03eb670a89d31
Musse-A <ayan.m4@icloud.com>
2025-09-27 20:25:44 -0400

    Title section of the hero section completed

--- vite-project/src/Components/Navbar.jsx
+++ vite-project/src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import {cn} from "@/lib/utils";
 import React, { useState, useEffect } from "react";
+import { X, Menu } from "lucide-react";
 import Logo from '@/assets/images/Logo.png';
 
 
@@ -14,6 +15,7 @@ const navItems = [
 
 export const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     
     useEffect(() => {
         const handleScroll = () => {
@@ -22,6 +24,7 @@ export const Navbar = () => {
         window.addEventListener('scroll', handleScroll)
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
+    
     return(
     <nav className={cn(
         "fixed w-full z-40 transition-all duration-300",
@@ -29,12 +32,60 @@ export const Navbar = () => {
     )}
     >
         <div className="container flex items-center justify-between">
-            <a className="text-xl font-bold text-primary flex items-center">
+            <a className="text-xl font-bold text-primary flex items-center"
+            href="#hero"
+            >
                 <img src={Logo} alt="Logo" className="w-10 h-10 mr-2 rounded-full" />
                 <span className="relative z-10">
-                    <span className="text-glow">Ayan</span> Portfolio
+                    <span className="text-glow text-foreground">Ayan</span> {""} Portfolio
                 </span>
             </a>
+            {/* Desktop Nav */}
+
+            <div className="hidden md:flex space-x-8">
+                {navItems.map((item, key) => (
+                    <a 
+                    key={key} 
+                    href={item.href} 
+                    className="text-foreground/80 hover:text-primary transition-colors duration 300"
+                    >
+                        {item.name}
+                    </a>
+                   
+                ))}
+            </div>
+
+            {/* Mobile Nav */}
+            <button onClick={() => setIsMenuOpen(prev => !prev)} className="md:hidden p-2 text-foreground z-50">
+                {""}
+                {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+                {""}
+            </button>
+            <div>
+                <div 
+                className={cn(
+                "fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+                "transition-all duration-300 md:hidden",
+                isMenuOpen ? "opacity-100 pointer-events-auto" :
+                 "opacity-0 pointer-events-none"
+
+                )}
+             >
+             <div className="flex flex-col space-y-8 text-xl"></div>
+                {navItems.map((item, key) => (
+                    <a 
+                    key={key} 
+                    href={item.href} 
+                    className="text-foreground/80 hover:text-primary transition-colors duration 300"
+                    onClick={() => setIsMenuOpen(false)}
+                    >
+                        {item.name}
+                    </a>
+                   
+                ))}
+            </div>
+            </div>
+
         </div>
     </nav>
     );
--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "../Components/Navbar";
 import { ThemeToggle } from "../Components/ThemeToggle";
 import { StarBackground } from '@/Components/StarBackground';
+import { HeroSection } from "@/Components/HeroSection";
 
 export const Home = () => {
     return (
@@ -16,7 +17,9 @@ export const Home = () => {
             <Navbar />
 
             {/* Main Content  */}
-
+            <main>
+                <HeroSection/>
+            </main>
             {/* Footer */}
 
         </div>
